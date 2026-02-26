6da2de18f31f1164678831d854db04c1e848719f
ankitkumar172022 <singhakash201401@gmail.com>
2026-02-26 16:01:11 +0530

    month.c

---
+++ month.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(){
+    int month;
+    printf("enter the number of the month");
+    scanf(" %d" &month);
+    if(month==1)
+    printf("31 days");
+    else if(month==2)
+    printf("28 days");
+    else if(month==3)
+    printf("31 days");
+    else if(month==4)
+    printf("30 days");
+    else if(month==5)
+    printf("31 days");
+    else if(month==6)
+    printf("30 days");
+    else if(month==7)
+    printf("31 days");
+    else if(month==8)
+    printf("31 days");
+    else if(month==9)
+    printf("30 days");
+    else if(month==10)
+    printf("31 days");
+    else if(month==11)
+    printf("30 days");
+    else if(month==12)
+    printf("31 days");
+    else
+    printf("invalid");
+    return 0;
+    
+}
